42d4eab7dcf1e2440c17b8abb824c6e5bc77e1a1
Catzilla <jewaysongonzalgo@gmail.com>
2022-01-30 18:12:37 +0800

    Add readme instructions on usage

--- README.md
+++ README.md
@@ -8,3 +8,47 @@ Palindromes are one of the introductory problems we normally encounter
 during academic days, it helped us understand and practice our sense of
 string manipulation and array management.
 
+
+## Setup
+
+This project runs on `pipenv` and `python 3` so we recommend using `pipenv` in a python 3 environment.
+
+```shell
+$ pipenv install
+```
+
+## Usage
+
+**Level 1: Checking if a string is a palindrome**
+
+```shell
+$ pipenv run palindrome --level=1 --string=aba
+
+level: 1
+string: aba
+result: True
+```
+
+**Level 2: Get the longest palindrome in a string**
+
+```shell
+$ pipenv run palindrome --level=2 --string=asabada
+
+level: 2
+string: asabada
+result: aba
+```
+
+**Level 3: Split string to palindromes**
+
+```shell
+$ pipenv run palindrome --level=3 --string=asabada
+
+level: 3
+string: asabada
+result: ['s', 'aba', 'ada']
+```
+
+
+## Palindrome functionalities
+
--- src/cli/commands.py
+++ src/cli/commands.py
@@ -7,25 +7,25 @@ from src.palindrome.divide_palindrome import divide_palindrome
 
 levels = '''set what palindrome function to use\n
 1: check if word is palindrome\n
-2: get palindrome in a word\n
-3: divide palindromes in a word
+2: get palindrome in a string\n
+3: divide palindromes in a string
 '''
 
 
 @click.command()
 @click.option('--level', default=1, help=levels)
-@click.option('--word', default='hello', help='the word that will be checked against palindrome function')
-def palindrome(level, word):
+@click.option('--string', default='hello', help='the string that will be checked against palindrome function')
+def palindrome(level, string):
     try:
         palindrome_methods = {
             1: is_palindrome,
             2: get_longest_palindrome,
             3: divide_palindrome,
         }
-        output = palindrome_methods[level](word)
+        output = palindrome_methods[level](string)
 
         click.echo(f'level: {level}')
-        click.echo(f'word: {word}')
+        click.echo(f'string: {string}')
         click.echo(f'result: {output}')
     except Exception as e:
         click.echo(f'Failed to run palindrome function {e}')
